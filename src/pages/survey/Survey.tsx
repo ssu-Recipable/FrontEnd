6bea4093607f9bb0732f073a7cf033a4d16bbfa8
dvp-tae <th3016@naver.com>
2024-04-25 19:11:14 +0900

    test: 폰트/색상 테스트

--- src/pages/survey/Survey.tsx
+++ src/pages/survey/Survey.tsx
@@ -1,23 +1,23 @@
 import Button from "@/components/Button";
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Survey = () => {
   return (
     <>
       <Wrapper>
         <SurveySection>
-            <h1>어떤 셰프의 레시피가 더 만족스러우셨나요?</h1>
-            <h2>서비스 개선에 큰 도움이 됩니다.</h2>
-            <Button>Chat GPT</Button>
-            <Button>Gemini</Button>
-            <h1>기타 의견을 작성해주세요.</h1>
-            <textarea />
+          <h1>어떤 셰프의 레시피가 더 만족스러우셨나요?</h1>
+          <h2>서비스 개선에 큰 도움이 됩니다.</h2>
+          <Button>Chat GPT</Button>
+          <Button>Gemini</Button>
+          <h1>기타 의견을 작성해주세요.</h1>
+          <textarea />
         </SurveySection>
         <ButtonSection>
-            <Link to="/">
-                <Button>홈으로</Button>
-            </Link>
+          <Link to="/">
+            <Button>홈으로</Button>
+          </Link>
         </ButtonSection>
       </Wrapper>
     </>
@@ -27,13 +27,12 @@ const Survey = () => {
 export default Survey;
 
 const Wrapper = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
-const SurveySection = styled.section`
-`;
+const SurveySection = styled.section``;
 
 const ButtonSection = styled.section`
-    position: fixed;
-    bottom: 1.2rem;
+  position: fixed;
+  bottom: 1.2rem;
 `;
