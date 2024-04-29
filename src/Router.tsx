4ff1056212317d029eb02441d509bf7b769adf59
dvp-tae <th3016@naver.com>
2024-04-29 22:48:30 +0900

    feat: 마이페이지 UI 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import Survey from "@/pages/survey/Survey";
 import Test from "@/pages/test/Test";
 import Login from "./pages/login/Login";
 import Main from "./pages/main/Main";
+import MyPage from "./pages/mypage/MyPage";
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/survey" element={<Survey />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/mypage/MyPage.tsx
@@ -0,0 +1,71 @@
+import Button from "@/components/commonComponents/Button";
+import Text from "@/components/commonComponents/Text";
+import { theme } from "@/styles/theme";
+import { FaUserCircle } from "react-icons/fa";
+import styled from "styled-components";
+
+const MyPage = () => {
+  return (
+    <MyPageContainer>
+      <MyPageTitle>
+        <Text font={"title1"}>마이페이지</Text>
+      </MyPageTitle>
+      <UserInfo>
+        <FaUserCircle size={90} color={theme.colors.grey2} />
+        <Text font={"title3"}>차현수</Text>
+      </UserInfo>
+      <MyPageMenu>
+        <div>
+          <Text font={"body1"}>프로필 수정</Text>
+        </div>
+        <span
+          style={{ border: `0.5px solid ${theme.colors.grey1}`, width: "100%" }}
+        />
+        <div>
+          <Text font={"body1"}>회원 탈퇴</Text>
+        </div>
+      </MyPageMenu>
+      <Button typeState={"defaultBtn"}>
+        <Text font={"button1"}>로그아웃</Text>
+      </Button>
+    </MyPageContainer>
+  );
+};
+
+const MyPageContainer = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const MyPageTitle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${theme.colors.grey2};
+  margin-top: 5rem;
+  padding: 1rem;
+`;
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 7rem;
+`;
+const MyPageMenu = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid ${theme.colors.grey1};
+  border-bottom: 1px solid ${theme.colors.grey1};
+  margin: 7rem 0;
+  div {
+    padding: 0.8rem;
+  }
+`;
+export default MyPage;
