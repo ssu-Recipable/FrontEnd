885b69c603470735cdce715764c854e28dc94232
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-05-09 18:17:14 +0900

    test: chatGPT api 테스트

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,7 @@ import EditProfile from "./pages/editProfile/EditProfile";
 import AddIngredient from "./pages/addIngredient/AddIngredient";
 import ScanReceipt from "./pages/scanReceipt/ScanReceipt";
 import IngredientInput from "./pages/ingredientInput/IngredientInput";
+import RecommendRecipes from "@/pages/recommendRecipes/RecommendRecipes"
 
 const Router = () => {
   return (
@@ -22,6 +23,7 @@ const Router = () => {
         <Route path="/addingredient" element={<AddIngredient />} />
         <Route path="/scanreceipt" element={<ScanReceipt />} />
         <Route path="/ingredientinput" element={<IngredientInput />} />
+        <Route path="/recommendRecipes" element={<RecommendRecipes />} />
       </Routes>
     </BrowserRouter>
   );
