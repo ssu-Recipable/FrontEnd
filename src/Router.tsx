1aec251fbf0a4af19924d18d7acddba863908a6c
dvp-tae <th3016@naver.com>
2024-05-12 21:11:58 +0900

    fix:충돌 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,45 +8,34 @@ import EditProfile from "./pages/editProfile/EditProfile";
 import AddIngredient from "./pages/addIngredient/AddIngredient";
 import ScanReceipt from "./pages/scanReceipt/ScanReceipt";
 import IngredientInput from "./pages/ingredientInput/IngredientInput";
-import RecommendRecipes from "@/pages/recommendRecipes/RecommendRecipes";
+import RecommendedRecipes from "@/pages/recommendedRecipes/RecommendedRecipes";
 import ResultScanReceipt from "./pages/resultScanReceipt/ResultScanReceipt";
 import LoginHandler from "./pages/login/components/LoginHandler";
 import Refrigerator from "./pages/refrigerator/Refrigerator";
 import ViewIngredient from "./pages/viewIngredient/ViewIngredient";
 import EditIngredient from "@/pages/editIngredient/EditIngredient";
-import { useRecoilValue } from "recoil";
-import { loginState } from "./recoil/atom";
+import Filtering from "./pages/filtering/Filtering";
 
 const Router = () => {
-  const isLogin = useRecoilValue(loginState);
   return (
     <BrowserRouter>
       <Routes>
-        {!isLogin ? (
-          <>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/login/oauth2/callback/kakao"
-              element={<LoginHandler />}
-            />
-          </>
-        ) : (
-          <>
-            <Route path="/test" element={<Test />} />
-            <Route path="/survey" element={<Survey />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/editprofile" element={<EditProfile />} />
-            <Route path="/addingredient" element={<AddIngredient />} />
-            <Route path="/scanreceipt" element={<ScanReceipt />} />
-            <Route path="/ingredientinput" element={<IngredientInput />} />
-            <Route path="/recommendRecipes" element={<RecommendRecipes />} />
-            <Route path="/resultscan" element={<ResultScanReceipt />} />
-            <Route path="/refrigerator" element={<Refrigerator />} />
-            <Route path="/ingredient/:id" element={<ViewIngredient />} />
-            <Route path="/editIngredient/:id" element={<EditIngredient />} />
-          </>
-        )}
+        <Route path="/test" element={<Test />} />
+        <Route path="/survey" element={<Survey />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login/oauth2/callback/kakao" element={<LoginHandler />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/addingredient" element={<AddIngredient />} />
+        <Route path="/scanreceipt" element={<ScanReceipt />} />
+        <Route path="/ingredientinput" element={<IngredientInput />} />
+        <Route path="/resultscan" element={<ResultScanReceipt />} />
+        <Route path="/refrigerator" element={<Refrigerator />} />
+        <Route path="/ingredient/:id" element={<ViewIngredient />} />
+        <Route path="/editIngredient/:id" element={<EditIngredient />} />
+        <Route path="/filtering" element={<Filtering />} />
+        <Route path="/recommendedRecipes" element={<RecommendedRecipes />} />
       </Routes>
     </BrowserRouter>
   );
