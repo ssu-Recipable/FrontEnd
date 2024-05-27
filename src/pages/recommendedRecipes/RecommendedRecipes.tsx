defa567b9e7b8e8126a9dec951b1eacd7b9aeecf
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-05-27 15:34:01 +0900

    fix: 상세 레시피 조회 경로 수정

--- src/pages/recommendedRecipes/RecommendedRecipes.tsx
+++ src/pages/recommendedRecipes/RecommendedRecipes.tsx
@@ -18,7 +18,7 @@ const RecommendedRecipes = () => {
                             <Text font={"title3"}>chatGPT 셰프의 추천 레시피</Text>
                         </div>
                         <RecipesList>
-                            <Link to={"/recommended/1"}>
+                            <Link to={"/recommendedRecipes/1"}>
                                 <Recipe>
                                     <img style={{width: "10.5rem", height: "8rem", background:"rgba(0, 0, 0, 0.1)"}}/>
                                     <RecipeInfo>
