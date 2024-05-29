28f4fcba106b49fff558d1193453c830f51fe58e
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-05-30 00:46:58 +0900

    fix: RefrigeratorApi.ts 파일 수정

--- src/utils/apis/RefrigeratorApi.ts
+++ src/utils/apis/RefrigeratorApi.ts
@@ -1,8 +1,16 @@
 import { RefrigeratorResponse } from "@/types/RefrigeratorType";
+import { IngredientRequset } from "@/types/ScanReceiptType";
 import { api } from "./axios";
-
 
 export const RefrigeratorApi = async () => {
     const response = await api.get<RefrigeratorResponse>('/refrigerators');
     return response.data.data.refrigeratorList;
 }
+
+export const AddRefrigeratorReceipt = (ingredients: IngredientRequset) => {
+    return api.post<string>("/refrigerators/receipt", ingredients);
+  };
+  
+  export const AddRefrigeratorDirect = (ingredients: IngredientRequset) => {
+    return api.post<string>("/refrigerators", ingredients);
+  };
