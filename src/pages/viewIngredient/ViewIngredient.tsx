07498e08678347227209b16cc7765eaa49d8b66b
dvp-tae <th3016@naver.com>
2024-10-02 13:15:00 +0900

    feat: pwa 테스트

--- src/pages/viewIngredient/ViewIngredient.tsx
+++ src/pages/viewIngredient/ViewIngredient.tsx
@@ -4,110 +4,116 @@ import styled from "styled-components";
 import Button from "@/components/commonComponents/Button";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { DeleteIngridientApi, ViewIngredientApi } from "@/utils/apis/IngredientApi";
+import {
+  DeleteIngridientApi,
+  ViewIngredientApi,
+} from "@/utils/apis/IngredientApi";
 import DefaultIngredientImg from "@/assets/images/default_ingredients.png";
 
 const ViewIngredient = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const { data } = useQuery({queryKey: ['ingredient', id], queryFn: () => id? ViewIngredientApi(id) : null});
-    const navigate = useNavigate();
+  const { data } = useQuery({
+    queryKey: ["ingredient", id],
+    queryFn: () => (id ? ViewIngredientApi(id) : null),
+  });
+  const navigate = useNavigate();
 
-    console.log(data);
+  console.log(data);
 
-    const deleteIngredient = async () => {
-        if(!id) {
-            return;
-        }
-
-        const confirmDelete = window.confirm('정말로 삭제하시겠습니까?');
-        if(!confirmDelete) {
-            return;
-        }
-
-        await DeleteIngridientApi(id);
-
-        navigate('/refrigerator');
+  const deleteIngredient = async () => {
+    if (!id) {
+      return;
     }
 
-    return (
-        <>
-            <RefrigeratorHeader />
-            <Wrapper>
-                {data?.ingredientImage === null ? <DefaultImg src={DefaultIngredientImg}/>
-                : <IngredientImg src={data?.ingredientImage} />}
-                <InfoSection>
-                    <Info>
-                        <Text font={"title3"}>이름</Text>
-                        <Text font={"body1"}>{data?.ingredientName}</Text>
-                    </Info>
-                    <Info>
-                        <Text font={"title3"}>카테고리</Text>
-                        <Text font={"body1"}>{data?.categoryName}</Text>
-                    </Info>
-                    <Info>
-                        <Text font={"title3"}>소비기한</Text>
-                        <Text font={"body1"}>{data?.expirationDay}</Text>
-                    </Info>
-                    <Info>
-                        <Text font={"title3"}>메모</Text>
-                        <Text font={"body1"}>{data?.memo}</Text>
-                    </Info>
-                </InfoSection>
-                <ButtonSection>
-                    <Link to={`/editIngredient/${id}`}>
-                        <Button typeState={"completeBtn"}>
-                            <Text font={"button2"}>재료 수정하기</Text>
-                        </Button>
-                    </Link>
-                    <Button typeState={"disproveBtn"} onClick={deleteIngredient}>
-                        <Text font={"button2"}>재료 삭제하기</Text>
-                    </Button>
-                </ButtonSection>
-            </Wrapper>
-        </>
-    );
-}
+    const confirmDelete = window.confirm("정말로 삭제하시겠습니까?");
+    if (!confirmDelete) {
+      return;
+    }
+
+    await DeleteIngridientApi(id);
+
+    navigate("/refrigerator");
+  };
+
+  return (
+    <>
+      <RefrigeratorHeader />
+      <Wrapper>
+        {data?.ingredientImage === null ? (
+          <DefaultImg src={DefaultIngredientImg} />
+        ) : (
+          <IngredientImg src={data?.ingredientImage} />
+        )}
+        <InfoSection>
+          <Info>
+            <Text font={"title3"}>이름</Text>
+            <Text font={"body1"}>{data?.ingredientName}</Text>
+          </Info>
+          <Info>
+            <Text font={"title3"}>카테고리</Text>
+            <Text font={"body1"}>{data?.categoryName}</Text>
+          </Info>
+          <Info>
+            <Text font={"title3"}>소비기한</Text>
+            <Text font={"body1"}>{data?.expirationDay}</Text>
+          </Info>
+          <Info>
+            <Text font={"title3"}>메모</Text>
+            <Text font={"body1"}>{data?.memo}</Text>
+          </Info>
+        </InfoSection>
+        <ButtonSection>
+          <Link to={`/editIngredient/${id}`}>
+            <Button typeState={"completeBtn"}>
+              <Text font={"button2"}>재료 수정하기</Text>
+            </Button>
+          </Link>
+          <Button typeState={"disproveBtn"} onClick={deleteIngredient}>
+            <Text font={"button2"}>재료 삭제하기</Text>
+          </Button>
+        </ButtonSection>
+      </Wrapper>
+    </>
+  );
+};
 export default ViewIngredient;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 `;
 
 const DefaultImg = styled.img`
-    width: 10rem;
-    height: 10rem;
-    margin: 5rem 0;
+  width: 10rem;
+  height: 10rem;
+  margin: 5rem 0;
 `;
 
 const IngredientImg = styled.img`
-    width: 10rem;
-    height: 10rem;
-    border-radius: 5rem;
-    border: 0.1rem solid rgba(0, 0, 0, 0.1);
-    margin: 5rem 0;
-    object-fit: cover;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 5rem;
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  margin: 5rem 0;
+  object-fit: cover;
 `;
 
 const InfoSection = styled.div`
-    width: 100%;
-    padding: 0 4rem;
-    margin-bottom: 3rem;
+  width: 100%;
+  padding: 0 4rem;
+  margin-bottom: 3rem;
 `;
 
 const Info = styled.div`
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 `;
 
-const ButtonSection = styled.div`    
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    margin-bottom: 3rem;
+const ButtonSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 3rem;
 `;
-
-
-
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,50 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "recipable",
+        short_name: "recipable",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#485EEE",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
